eabf61eba1c9e6de858c25fbb363fcfb40eb0bab
Joseph Tam <josephnhtam@gmail.com>
2023-12-27 01:58:17 +0800

    feat: add RtmpMessageTypes

--- LiveStreamingServer/Rtmp/Core/RtmpMessagetHandler/PayloadHandling/RtmpMessages.cs
+++ LiveStreamingServer/Rtmp/Core/RtmpMessagetHandler/PayloadHandling/RtmpMessages.cs
@@ -1,21 +1,5 @@
 ﻿namespace LiveStreamingServer.Rtmp.Core.RtmpMessageHandler.PayloadHandling
 {
-    public static class RtmpMessages
-    {
-        public const byte SetChunkSize = 0x01;
-        public const byte AbortMessage = 0x02;
-        public const byte Acknowledgement = 0x03;
-        public const byte UserControlMessage = 0x04;
-        public const byte WindowAcknowledgementSize = 0x05;
-        public const byte SetPeerBandwidth = 0x06;
-        public const byte AudioMessage = 0x08;
-        public const byte VideoMessage = 0x09;
-        public const byte DataMessage3 = 0x0F;
-        public const byte SharedObjectMessage3 = 0x10;
-        public const byte CommandMessage3 = 0x11;
-        public const byte AggregateMessage = 0x16;
-    }
-
     public enum RtmpMessageTypes : byte
     {
         /* protocol control messages */
@@ -26,11 +10,27 @@
         SetPeerBandwidth = 6,
 
         /* user control messages */
-        AudioMessage = 0x08,
-        VideoMessage = 0x09,
-        DataMessage3 = 0x0F,
-        SharedObjectMessage3 = 0x10,
-        CommandMessage3 = 0x11,
-        AggregateMessage = 0x16
+        UserControlMessage = 4,
+
+        /* command messages */
+        CommandMessageAmf0 = 20,
+        CommandMessageAmf3 = 17,
+
+        /* data messages */
+        DataMessageAmf0 = 18,
+        DataMessageAmf3 = 15,
+
+        /* shared object messages */
+        SharedObjectMessageAmf0 = 19,
+        SharedObjectMessageAmf3 = 16,
+
+        /* audio message */
+        AudioMessage = 8,
+
+        /* video message */
+        VideoMessage = 9,
+
+        /* aggregate message */
+        AggregateMessage = 22,
     }
 }
