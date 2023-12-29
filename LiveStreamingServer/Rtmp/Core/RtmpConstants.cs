632624c4183afe9b7652f53175c1fcded96c6af2
Joseph Tam <josephnhtam@gmail.com>
2023-12-29 22:06:28 +0800

    refactor: rename PeerBandwidthLimitType to RtmpPeerBandwidthLimitType

--- LiveStreamingServer/Rtmp/Core/RtmpConstants.cs
+++ LiveStreamingServer/Rtmp/Core/RtmpConstants.cs
@@ -3,12 +3,15 @@
     public static class RtmpConstants
     {
         public const uint DefaultChunkSize = 128;
+        public const uint DefaultPeerBandwidth = 500_000;
+        public const uint DefaultInAcknowledgementWindowSize = 250_000;
+        public const uint DefaultOutAcknowledgementWindowSize = 250_000;
 
         public const uint ProtocolControlMessageChunkStreamId = 2;
         public const uint ProtocolControlMessageStreamId = 0;
     }
 
-    public enum PeerBandwidthLimitType : byte
+    public enum RtmpPeerBandwidthLimitType : byte
     {
         Hard = 0,
         Soft = 1,
