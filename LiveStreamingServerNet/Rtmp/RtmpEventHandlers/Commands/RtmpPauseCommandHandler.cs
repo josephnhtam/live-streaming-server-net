943e5ae238618c4367489c9df9b9623adf2ab9b3
Joseph Tam <josephnhtam@gmail.com>
2024-01-06 19:38:09 +0800

    feat: implement RtmpPauseCommandHandler

--- LiveStreamingServerNet/Rtmp/RtmpEventHandlers/Commands/RtmpPauseCommandHandler.cs
+++ LiveStreamingServerNet/Rtmp/RtmpEventHandlers/Commands/RtmpPauseCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using LiveStreamingServerNet.Rtmp.Contracts;
 using LiveStreamingServerNet.Rtmp.RtmpEventHandlers.CommandDispatcher;
 using LiveStreamingServerNet.Rtmp.RtmpEventHandlers.CommandDispatcher.Attributes;
+using LiveStreamingServerNet.Rtmp.Services.Contracts;
 
 namespace LiveStreamingServerNet.Rtmp.RtmpEventHandlers.Commands
 {
@@ -9,13 +10,59 @@ namespace LiveStreamingServerNet.Rtmp.RtmpEventHandlers.Commands
     [RtmpCommand("pause")]
     public class RtmpPauseCommandHandler : RtmpCommandHandler<RtmpPauseCommand>
     {
+        private readonly IRtmpUserControlMessageSenderService _userControlMessageSender;
+        private readonly IRtmpStreamManagerService _streamManager;
+        private readonly IRtmpMediaMessageManagerService _mediaMessageManager;
+
+        public RtmpPauseCommandHandler(
+            IRtmpUserControlMessageSenderService userControlMessageSender,
+            IRtmpStreamManagerService streamManager,
+            IRtmpMediaMessageManagerService mediaMessageManager)
+        {
+            _userControlMessageSender = userControlMessageSender;
+            _streamManager = streamManager;
+            _mediaMessageManager = mediaMessageManager;
+        }
+
         public override Task<bool> HandleAsync(
             IRtmpChunkStreamContext chunkStreamContext,
             IRtmpClientPeerContext peerContext,
             RtmpPauseCommand command,
             CancellationToken cancellationToken)
         {
+            if (command.Flag)
+                HandlePause(peerContext);
+            else
+                HandleUnpause(peerContext, chunkStreamContext);
+
             return Task.FromResult(true);
+        }
+
+        private void HandleUnpause(IRtmpClientPeerContext peerContext, IRtmpChunkStreamContext chunkStreamContext)
+        {
+            _userControlMessageSender.SendStreamBeginMessage(peerContext);
+            SendCachedHeaderMessages(peerContext, chunkStreamContext);
+        }
+
+        private void HandlePause(IRtmpClientPeerContext peerContext)
+        {
+            _userControlMessageSender.SendStreamEofMessage(peerContext);
+        }
+
+        private void SendCachedHeaderMessages(IRtmpClientPeerContext peerContext, IRtmpChunkStreamContext chunkStreamContext)
+        {
+            if (peerContext.StreamSubscriptionContext == null)
+                return;
+
+            var publishStreamContext = _streamManager.GetPublishStreamContext(peerContext.StreamSubscriptionContext.StreamPath);
+
+            if (publishStreamContext == null)
+                return;
+
+            _mediaMessageManager.SendCachedHeaderMessages(
+                peerContext, publishStreamContext,
+                chunkStreamContext.MessageHeader.Timestamp,
+                chunkStreamContext.MessageHeader.MessageStreamId);
         }
     }
 }
