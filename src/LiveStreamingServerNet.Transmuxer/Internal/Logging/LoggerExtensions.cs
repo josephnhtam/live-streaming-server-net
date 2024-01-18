249152f362a13ff15e4a0ff8eec99ab5c24e1618
Joseph Tam <josephnhtam@gmail.com>
2024-01-19 01:48:55 +0800

    feat: add LoggerExtensions

---
+++ src/LiveStreamingServerNet.Transmuxer/Internal/Logging/LoggerExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LiveStreamingServerNet.Transmuxer.Internal.Logging
+{
+    internal static partial class LoggerExtensions
+    {
+        [LoggerMessage(LogLevel.Debug, "Transmuxer Error | InputPath: {InputPath} | OutputDirPath: {OutputDirPath} | StreamPath: {StreamPath}")]
+        public static partial void TransmuxerError(this ILogger logger, string inputPath, string outputDirPath, string streamPath, Exception exception);
+    }
+}
