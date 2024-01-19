1ea575af285d4ed3230f62b3fb4c2bacf5fd06bf
Joseph Tam <josephnhtam@gmail.com>
2024-01-20 00:52:58 +0800

    refactor: ExecutableFinder

--- src/LiveStreamingServerNet.Transmuxer/Utilities/ExecutableFinder.cs
+++ src/LiveStreamingServerNet.Transmuxer/Utilities/ExecutableFinder.cs
@@ -1,18 +1,16 @@
-﻿namespace LiveStreamingServerNet.Transmuxer.Internal.Utilities
+﻿using System.Reflection;
+
+namespace LiveStreamingServerNet.Transmuxer.Internal.Utilities
 {
     public static class ExecutableFinder
     {
         public static string? FindExecutableFromPATH(string executableName)
         {
-            var paths = Environment.GetEnvironmentVariable("PATH")!.Split(Path.PathSeparator);
-
-            foreach (string path in paths)
+            foreach (string path in GetPaths())
             {
                 var executable = FindExecutable(path, executableName);
                 if (executable != null)
-                {
                     return executable.FullName;
-                }
             }
 
             return null;
@@ -20,12 +18,11 @@
 
         public static FileInfo? FindExecutable(string directoryPath, string executableName)
         {
-            if (Directory.Exists(directoryPath))
-            {
-                IEnumerable<FileInfo> files = new DirectoryInfo(directoryPath).GetFiles();
-                return DoFindExecutable(files, executableName);
-            }
-            return null;
+            if (!Directory.Exists(directoryPath))
+                return null;
+
+            IEnumerable<FileInfo> files = new DirectoryInfo(directoryPath).GetFiles();
+            return DoFindExecutable(files, executableName);
         }
 
         private static FileInfo? DoFindExecutable(IEnumerable<FileInfo> files, string executableName)
@@ -33,6 +30,18 @@
             return files.FirstOrDefault((x) =>
                 x.Name.Equals(executableName, StringComparison.InvariantCultureIgnoreCase) ||
                 x.Name.Equals(executableName + ".exe", StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        private static IEnumerable<string> GetPaths()
+        {
+            var pathEnvVar = Environment.GetEnvironmentVariable("PATH");
+            if (!string.IsNullOrEmpty(pathEnvVar))
+                foreach (var path in pathEnvVar.Split(Path.PathSeparator))
+                    yield return path;
+
+            var entryPath = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
+            if (!string.IsNullOrEmpty(entryPath))
+                yield return entryPath;
         }
     }
 }
