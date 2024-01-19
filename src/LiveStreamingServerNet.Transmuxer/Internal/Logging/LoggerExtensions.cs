5130208b3ab964c4a532b8e849d04c7369c54f21
Joseph Tam <josephnhtam@gmail.com>
2024-01-20 01:47:49 +0800

    feat: improve logging

--- src/LiveStreamingServerNet.Transmuxer/Internal/Logging/LoggerExtensions.cs
+++ src/LiveStreamingServerNet.Transmuxer/Internal/Logging/LoggerExtensions.cs
@@ -4,7 +4,13 @@ namespace LiveStreamingServerNet.Transmuxer.Internal.Logging
 {
     internal static partial class LoggerExtensions
     {
-        [LoggerMessage(LogLevel.Debug, "Transmuxer Error | InputPath: {InputPath} | OutputDirPath: {OutputDirPath} | StreamPath: {StreamPath}")]
-        public static partial void TransmuxerError(this ILogger logger, string inputPath, string outputDirPath, string streamPath, Exception exception);
+        [LoggerMessage(LogLevel.Information, "Transmuxer Started | InputPath: {InputPath} | OutputPath: {OutputPath} | StreamPath: {StreamPath}")]
+        public static partial void TransmuxerStarted(this ILogger logger, string inputPath, string outputPath, string streamPath);
+
+        [LoggerMessage(LogLevel.Information, "Transmuxer Stopped | InputPath: {InputPath} | OutputDirPath: {OutputPath} | StreamPath: {StreamPath}")]
+        public static partial void TransmuxerStopped(this ILogger logger, string inputPath, string outputPath, string streamPath);
+
+        [LoggerMessage(LogLevel.Error, "Transmuxer Error | InputPath: {InputPath} | OutputPath: {OutputPath} | StreamPath: {StreamPath}")]
+        public static partial void TransmuxerError(this ILogger logger, string inputPath, string outputPath, string streamPath, Exception exception);
     }
 }
