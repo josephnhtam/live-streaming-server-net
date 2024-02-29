b60f81f6bfafc939fab8303f4ecf9493ac82ac5c
Jordy van Essen <j.vanessen@viscon.eu>
2024-02-29 19:20:29 +0100

    Remove the usage of System.Security.Cryptography

--- src/LiveStreamingServerNet.Rtmp/Internal/RtmpServerContext.cs
+++ src/LiveStreamingServerNet.Rtmp/Internal/RtmpServerContext.cs
@@ -1,15 +1,21 @@
 ﻿using LiveStreamingServerNet.Rtmp.Contracts;
-using System.Security.Cryptography;
+using System.Text;
 
 namespace LiveStreamingServerNet.Rtmp.Internal
 {
     internal class RtmpServerContext : IRtmpServerContext
     {
-        public string AuthCode { get; }
+        public string AuthCode { get; } = GenerateRandomHexString(64);
 
-        public RtmpServerContext()
+        private static string GenerateRandomHexString(int length)
         {
-            AuthCode = RandomNumberGenerator.GetHexString(64);
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            var randomString = new string(Enumerable.Repeat(chars, length)
+                           .Select(s => s[random.Next(s.Length)]).ToArray());
+
+            var randomBytes = Encoding.UTF8.GetBytes(randomString);
+            return BitConverter.ToString(randomBytes).Replace("-", string.Empty);
         }
     }
 }
