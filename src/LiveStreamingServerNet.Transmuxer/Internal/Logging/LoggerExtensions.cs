b5f56ebaa7711ac9cdedda2fcd9f05303501e6af
Joseph Tam <josephnhtam@gmail.com>
2024-03-16 19:26:36 +0800

    feat: handle exception thrown during dispatching transmuxer events

--- src/LiveStreamingServerNet.Transmuxer/Internal/Logging/LoggerExtensions.cs
+++ src/LiveStreamingServerNet.Transmuxer/Internal/Logging/LoggerExtensions.cs
@@ -12,5 +12,11 @@ namespace LiveStreamingServerNet.Transmuxer.Internal.Logging
 
         [LoggerMessage(LogLevel.Error, "Transmuxer Error | InputPath: {InputPath} | StreamPath: {StreamPath}")]
         public static partial void TransmuxerError(this ILogger logger, string inputPath, string streamPath, Exception exception);
+
+        [LoggerMessage(LogLevel.Error, "An error occurred while dispatching transmuxer started event | Identifier: {Identifier} | InputPath: {InputPath} | OutputPath: {OutputPath} | StreamPath: {StreamPath}")]
+        public static partial void DispatchingTransmuxerStartedEventError(this ILogger logger, string identifier, string inputPath, string outputPath, string streamPath, Exception ex);
+
+        [LoggerMessage(LogLevel.Error, "An error occurred while dispatching transmuxer stopped event | Identifier: {Identifier} | InputPath: {InputPath} | OutputPath: {OutputPath} | StreamPath: {StreamPath}")]
+        public static partial void DispatchingTransmuxerStoppedEventError(this ILogger logger, string identifier, string inputPath, string outputPath, string streamPath, Exception ex);
     }
 }
--- src/LiveStreamingServerNet.Transmuxer/Internal/TransmuxerEventDispatcher.cs
+++ src/LiveStreamingServerNet.Transmuxer/Internal/TransmuxerEventDispatcher.cs
@@ -1,17 +1,22 @@
 ﻿using LiveStreamingServerNet.Transmuxer.Contracts;
 using LiveStreamingServerNet.Transmuxer.Internal.Contracts;
+using LiveStreamingServerNet.Transmuxer.Internal.Logging;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace LiveStreamingServerNet.Transmuxer.Internal
 {
     internal class TransmuxerEventDispatcher : ITransmuxerEventDispatcher
     {
         private readonly IServiceProvider _services;
+        private readonly ILogger<TransmuxerEventDispatcher> _logger;
+
         private ITransmuxerEventHandler[]? _eventHandlers;
 
-        public TransmuxerEventDispatcher(IServiceProvider services)
+        public TransmuxerEventDispatcher(IServiceProvider services, ILogger<TransmuxerEventDispatcher> logger)
         {
             _services = services;
+            _logger = logger;
         }
 
         public ITransmuxerEventHandler[] GetEventHandlers()
@@ -22,14 +27,28 @@ namespace LiveStreamingServerNet.Transmuxer.Internal
 
         public async Task TransmuxerStartedAsync(uint clientId, string identifier, string inputPath, string outputPath, string streamPath, IDictionary<string, string> streamArguments)
         {
-            foreach (var handler in GetEventHandlers())
-                await handler.OnTransmuxerStartedAsync(clientId, identifier, inputPath, outputPath, streamPath, streamArguments);
+            try
+            {
+                foreach (var handler in GetEventHandlers())
+                    await handler.OnTransmuxerStartedAsync(clientId, identifier, inputPath, outputPath, streamPath, streamArguments);
+            }
+            catch (Exception ex)
+            {
+                _logger.DispatchingTransmuxerStartedEventError(identifier, inputPath, outputPath, streamPath, ex);
+            }
         }
 
         public async Task TransmuxerStoppedAsync(uint clientId, string identifier, string inputPath, string outputPath, string streamPath, IDictionary<string, string> streamArguments)
         {
-            foreach (var handler in GetEventHandlers())
-                await handler.OnTransmuxerStoppedAsync(clientId, identifier, inputPath, outputPath, streamPath, streamArguments);
+            try
+            {
+                foreach (var handler in GetEventHandlers())
+                    await handler.OnTransmuxerStoppedAsync(clientId, identifier, inputPath, outputPath, streamPath, streamArguments);
+            }
+            catch (Exception ex)
+            {
+                _logger.DispatchingTransmuxerStoppedEventError(identifier, inputPath, outputPath, streamPath, ex);
+            }
         }
     }
 }
