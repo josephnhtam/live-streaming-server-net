795ec46dca2477a2bd138469df916f9a5ac0e503
Joseph Tam <josephnhtam@gmail.com>
2024-03-17 19:19:16 +0800

    refactor: move PodPatcherBuilder to another namespace

---
+++ src/LiveStreamingServerNet/KubernetesPod/Utilities/Contracts/IPodPatcherBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace LiveStreamingServerNet.KubernetesPod.Utilities.Contracts
+{
+    public interface IPodPatcherBuilder
+    {
+        IPodPatcherBuilder SetLabel(string key, string value);
+        IPodPatcherBuilder RemoveLabel(string key);
+        IPodPatcherBuilder SetAnnotation(string key, string value);
+        IPodPatcherBuilder RemoveAnnotation(string key);
+    }
+}
