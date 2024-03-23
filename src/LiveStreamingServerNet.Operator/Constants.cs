dbb4f10c31eada4427446009cd77da49c2b9a720
Joseph Tam <josephnhtam@gmail.com>
2024-03-23 20:56:30 +0800

    feat: add streams limit

--- src/LiveStreamingServerNet.Operator/Constants.cs
+++ src/LiveStreamingServerNet.Operator/Constants.cs
@@ -6,6 +6,11 @@
         public const string AppLabelValue = "live-streaming-server-net";
 
         public const string PendingStopLabel = "live-streaming-server-net/pending-stop";
+        public const string LimitReachedLabel = "live-streaming-server-net/limit-reached";
+
         public const string StreamsCountAnnotation = "live-streaming-server-net/streams-count";
+        public const string StreamsLimitAnnotation = "live-streaming-server-net/streams-limit";
+
+        public const string StreamsLimitEnv = "STREAMS_LIMIT";
     }
 }
