0edf3ae6c8b849256946d4cf5d4516e8dfe6042a
Joseph Tam <josephnhtam@gmail.com>
2024-03-25 01:07:42 +0800

    feat: add IKubernetesPodConfigurator

---
+++ src/LiveStreamingServerNet.KubernetesPod/Installer/KubernetesPodConfigurator.cs
@@ -0,0 +1,23 @@
+﻿using LiveStreamingServerNet.KubernetesPod.Configurations;
+using LiveStreamingServerNet.KubernetesPod.Installer.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LiveStreamingServerNet.KubernetesPod.Installer
+{
+    public class KubernetesPodConfigurator : IKubernetesPodConfigurator
+    {
+        public IServiceCollection Services { get; }
+
+        public KubernetesPodConfigurator(IServiceCollection services)
+        {
+            Services = services;
+        }
+
+        public IKubernetesPodConfigurator Configure(Action<KubernetesPodConfiguration>? configure)
+        {
+            if (configure != null)
+                Services.Configure(configure);
+            return this;
+        }
+    }
+}
