68b5fd84b87912930dd7bdfb14a2e21c6399b885
Joseph Tam <josephnhtam@gmail.com>
2024-04-10 20:43:15 +0800

    feat: add LiveStreamingServerNet.AuthorizationDemo

---
+++ samples/LiveStreamingServerNet.AuthorizationDemo/Program.cs
@@ -0,0 +1,86 @@
+﻿using LiveStreamingServerNet.Networking;
+using LiveStreamingServerNet.Networking.Contracts;
+using LiveStreamingServerNet.Rtmp.Auth;
+using LiveStreamingServerNet.Rtmp.Auth.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace LiveStreamingServerNet.RtmpsDemo
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            using var liveStreamingServer = CreateLiveStreamingServer();
+
+            using var cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                cts.Cancel();
+                e.Cancel = true;
+            };
+
+            await liveStreamingServer.RunAsync(
+                new ServerEndPoint(new IPEndPoint(IPAddress.Any, 1935), false), cts.Token);
+        }
+
+        private static ILiveStreamingServer CreateLiveStreamingServer()
+        {
+            return LiveStreamingServerBuilder.Create()
+                .ConfigureRtmpServer(options =>
+                {
+                    options.Services.AddSingleton<IPasswordValidator, DemoPasswordValidator>();
+                    options.AddAuthorizationHandler<DemoAuthorizationHandler>();
+                })
+                .ConfigureLogging(options => options.AddConsole().SetMinimumLevel(LogLevel.Debug))
+                .Build();
+        }
+    }
+
+    public interface IPasswordValidator
+    {
+        bool ValidatePassowrd(string password);
+    }
+
+    public class DemoPasswordValidator : IPasswordValidator
+    {
+        public bool ValidatePassowrd(string password)
+        {
+            return password == "123456";
+        }
+    }
+
+    public class DemoAuthorizationHandler : IAuthorizationHandler
+    {
+        private readonly IPasswordValidator _passwordValidator;
+
+        public DemoAuthorizationHandler(IPasswordValidator passwordValidator)
+        {
+            _passwordValidator = passwordValidator;
+        }
+
+        public Task<AuthorizationResult> AuthorizePublishingAsync(
+            IClientInfo client,
+            string streamPath,
+            IReadOnlyDictionary<string, string> streamArguments,
+            string publishingType)
+        {
+            // Accepting only the publishing path that includes a valid password parameter
+            // For example: rtmp://127.0.0.1:1935/live/stream?password=123456
+            if (streamArguments.TryGetValue("password", out var password) && _passwordValidator.ValidatePassowrd(password))
+                return Task.FromResult(AuthorizationResult.Authorized());
+
+            return Task.FromResult(AuthorizationResult.Unauthorized("incorrect password"));
+        }
+
+        public Task<AuthorizationResult> AuthorizeSubscriptionAsync(
+            IClientInfo client,
+            string streamPath,
+            IReadOnlyDictionary<string, string> streamArguments)
+        {
+            return Task.FromResult(AuthorizationResult.Authorized());
+        }
+    }
+}
