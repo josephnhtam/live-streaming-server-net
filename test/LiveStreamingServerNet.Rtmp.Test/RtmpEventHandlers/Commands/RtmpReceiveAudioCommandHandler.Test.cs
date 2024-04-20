32a690a05b079f437cc08a351333e9fcb2615307
Joseph Tam <josephnhtam@gmail.com>
2024-04-20 18:34:28 +0800

    feat: add unit tests

---
+++ test/LiveStreamingServerNet.Rtmp.Test/RtmpEventHandlers/Commands/RtmpReceiveAudioCommandHandler.Test.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using LiveStreamingServerNet.Rtmp.Internal.Contracts;
+using LiveStreamingServerNet.Rtmp.Internal.RtmpEventHandlers.Commands;
+using NSubstitute;
+
+namespace LiveStreamingServerNet.Rtmp.Test.RtmpEventHandlers.Commands
+{
+    public class RtmpReceiveAudioCommandHandlerTest
+    {
+        private readonly IRtmpClientContext _clientContext;
+        private readonly IRtmpChunkStreamContext _chunkStreamContext;
+        private readonly RtmpReceiveAudioCommandHandler _sut;
+
+        public RtmpReceiveAudioCommandHandlerTest()
+        {
+            _clientContext = Substitute.For<IRtmpClientContext>();
+            _chunkStreamContext = Substitute.For<IRtmpChunkStreamContext>();
+            _sut = new RtmpReceiveAudioCommandHandler();
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task HandleAsync_Should_SetIsReceivingAudio_When_SubscriptionContextNotNull(bool flag)
+        {
+            // Arrange
+            var command = new RtmpReceiveAudioCommand(0, new Dictionary<string, object>(), flag);
+
+            var subscriptionContext = Substitute.For<IRtmpStreamSubscriptionContext>();
+
+            _clientContext.StreamSubscriptionContext.Returns(subscriptionContext);
+
+            // Act
+            var result = await _sut.HandleAsync(_chunkStreamContext, _clientContext, command, CancellationToken.None);
+
+            // Assert
+            subscriptionContext.Received(1).IsReceivingAudio = flag;
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task HandleAsync_Should_StillReturnTrue_When_SubscriptionContextNull(bool flag)
+        {
+            // Arrange
+            var command = new RtmpReceiveAudioCommand(0, new Dictionary<string, object>(), flag);
+
+            _clientContext.StreamSubscriptionContext.Returns((IRtmpStreamSubscriptionContext?)null);
+
+            // Act
+            var result = await _sut.HandleAsync(_chunkStreamContext, _clientContext, command, CancellationToken.None);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+    }
+}
---
+++ test/LiveStreamingServerNet.Rtmp.Test/RtmpEventHandlers/Commands/RtmpReceiveVideoCommandHandler.Test.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using LiveStreamingServerNet.Rtmp.Internal.Contracts;
+using LiveStreamingServerNet.Rtmp.Internal.RtmpEventHandlers.Commands;
+using NSubstitute;
+
+namespace LiveStreamingServerNet.Rtmp.Test.RtmpEventHandlers.Commands
+{
+    public class RtmpReceiveVideoCommandHandlerTest
+    {
+        private readonly IRtmpClientContext _clientContext;
+        private readonly IRtmpChunkStreamContext _chunkStreamContext;
+        private readonly RtmpReceiveVideoCommandHandler _sut;
+
+        public RtmpReceiveVideoCommandHandlerTest()
+        {
+            _clientContext = Substitute.For<IRtmpClientContext>();
+            _chunkStreamContext = Substitute.For<IRtmpChunkStreamContext>();
+            _sut = new RtmpReceiveVideoCommandHandler();
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task HandleAsync_Should_SetIsReceivingVideo_When_SubscriptionContextNotNull(bool flag)
+        {
+            // Arrange
+            var command = new RtmpReceiveVideoCommand(0, new Dictionary<string, object>(), flag);
+
+            var subscriptionContext = Substitute.For<IRtmpStreamSubscriptionContext>();
+
+            _clientContext.StreamSubscriptionContext.Returns(subscriptionContext);
+
+            // Act
+            var result = await _sut.HandleAsync(_chunkStreamContext, _clientContext, command, CancellationToken.None);
+
+            // Assert
+            subscriptionContext.Received(1).IsReceivingVideo = flag;
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task HandleAsync_Should_StillReturnTrue_When_SubscriptionContextNull(bool flag)
+        {
+            // Arrange
+            var command = new RtmpReceiveVideoCommand(0, new Dictionary<string, object>(), flag);
+
+            _clientContext.StreamSubscriptionContext.Returns((IRtmpStreamSubscriptionContext?)null);
+
+            // Act
+            var result = await _sut.HandleAsync(_chunkStreamContext, _clientContext, command, CancellationToken.None);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+    }
+}
