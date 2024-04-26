c065ee58b8fbc9ca2d6c17e3dd6698e22fb35ffe
Joseph Tam <josephnhtam@gmail.com>
2024-04-27 04:12:17 +0800

    fix: amf3 object writer

--- src/LiveStreamingServerNet.Rtmp/Internal/Extensions/AmfWriterExtensions.cs
+++ src/LiveStreamingServerNet.Rtmp/Internal/Extensions/AmfWriterExtensions.cs
@@ -6,18 +6,23 @@ namespace LiveStreamingServerNet.Rtmp.Internal.Extensions
     {
         public static void WriteAmf0ObjectWithType(this AmfWriter amfWriter, IEnumerable<KeyValuePair<string, object?>> keyValuePairs)
         {
-            var amf0Object = new Amf0Object();
-            amf0Object.ClassName = string.Empty;
-            amf0Object.DynamicMembersAndValues = keyValuePairs as Dictionary<string, object?> ?? new Dictionary<string, object?>(keyValuePairs);
+            var amf0Object = new Amf0Object()
+            {
+                ClassName = string.Empty,
+                DynamicMembersAndValues = keyValuePairs as Dictionary<string, object?> ?? new Dictionary<string, object?>(keyValuePairs)
+            };
             amfWriter.WriteAmf0(amf0Object);
         }
 
         public static void WriteAmf3ObjectWithType(this AmfWriter amfWriter, IEnumerable<KeyValuePair<string, object?>> keyValuePairs)
         {
-            var amf0Object = new Amf3Object();
-            amf0Object.Trait.IsDynamic = true;
-            amf0Object.DynamicMembersAndValues = keyValuePairs as Dictionary<string, object?> ?? new Dictionary<string, object?>(keyValuePairs);
-            amfWriter.WriteAmf3(amf0Object);
+            var amf3Object = new Amf3Object()
+            {
+                Values = new List<object?>(),
+                Trait = new Amf3Trait { ClassName = string.Empty, IsDynamic = true, Members = new List<string>() },
+                DynamicMembersAndValues = keyValuePairs as Dictionary<string, object?> ?? new Dictionary<string, object?>(keyValuePairs)
+            };
+            amfWriter.WriteAmf3(amf3Object);
         }
     }
 }
