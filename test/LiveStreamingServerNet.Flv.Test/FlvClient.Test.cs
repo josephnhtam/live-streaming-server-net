18f11c569f0802725e7a29afcdf28ca767acc6e2
Joseph Tam <josephnhtam@gmail.com>
2024-04-28 19:47:32 +0800

    feat: add unit tests for FlvClient

---
+++ test/LiveStreamingServerNet.Flv.Test/FlvClient.Test.cs
@@ -0,0 +1,173 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using LiveStreamingServerNet.Flv.Internal;
+using LiveStreamingServerNet.Flv.Internal.Contracts;
+using LiveStreamingServerNet.Flv.Internal.Services.Contracts;
+using LiveStreamingServerNet.Networking.Contracts;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+
+namespace LiveStreamingServerNet.Flv.Test
+{
+    public class FlvClientTest
+    {
+        private readonly IFixture _fixture;
+        private readonly string _clientId;
+        private readonly string _streamPath;
+        private readonly CancellationTokenSource _stoppingCts;
+        private readonly CancellationToken _stoppingToken;
+        private readonly IFlvMediaTagManagerService _mediaTagManager;
+        private readonly IFlvWriter _flvWriter;
+        private readonly ILogger<FlvClient> _logger;
+        private readonly IFlvClient _sut;
+
+        public FlvClientTest()
+        {
+            _fixture = new Fixture();
+            _clientId = _fixture.Create<string>();
+            _streamPath = _fixture.Create<string>();
+            _stoppingCts = new CancellationTokenSource();
+            _stoppingToken = _stoppingCts.Token;
+
+            _mediaTagManager = Substitute.For<IFlvMediaTagManagerService>();
+            _flvWriter = Substitute.For<IFlvWriter>();
+            _logger = Substitute.For<ILogger<FlvClient>>();
+            _sut = new FlvClient(_clientId, _streamPath, _mediaTagManager, _flvWriter, _logger, _stoppingToken);
+        }
+
+        [Fact]
+        public void Constructor_Should_RegisterClient_WithMediaTagManager()
+        {
+            // Assert
+            _mediaTagManager.Received(1).RegisterClient(_sut);
+
+            _sut.UntilIntializationComplete().IsCompleted.Should().BeFalse();
+            _sut.UntilComplete().IsCompleted.Should().BeFalse();
+        }
+
+        [Fact]
+        public void CompleteInitialization_Should_MarkInitializationComplete()
+        {
+            // Act
+            _sut.CompleteInitialization();
+
+            // Assert
+            _sut.UntilIntializationComplete().IsCompleted.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Stop_Should_CancelInitializationAndMarkComplete()
+        {
+            // Act
+            _sut.Stop();
+
+            // Assert
+            _sut.UntilIntializationComplete().IsCanceled.Should().BeTrue();
+            _sut.UntilComplete().IsCompleted.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CancellingStoppingToken_Should_CancelInitializationAndMarkComplete()
+        {
+            // Act
+            _stoppingCts.Cancel();
+
+            // Assert
+            _sut.UntilIntializationComplete().IsCanceled.Should().BeTrue();
+            _sut.UntilComplete().IsCompleted.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task DisposeAsync_Should_UnregisterClient_FromMediaTagManager()
+        {
+            // Act
+            await _sut.DisposeAsync();
+
+            // Assert
+            _mediaTagManager.Received(1).UnregisterClient(_sut);
+        }
+
+        [Fact]
+        public async Task DisposeAsync_Should_CancelInitializationAndMarkComplete()
+        {
+            // Act
+            await _sut.DisposeAsync();
+
+            // Assert
+            _sut.UntilIntializationComplete().IsCanceled.Should().BeTrue();
+            _sut.UntilComplete().IsCompleted.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task DisposeAsync_Should_DisposeFlvWriter()
+        {
+            // Act
+            await _sut.DisposeAsync();
+
+            // Assert
+            await _flvWriter.Received(1).DisposeAsync();
+        }
+
+
+        [Fact]
+        public async Task WriteHeaderAsync_Should_WriteHeader()
+        {
+            // Arrange
+            var allowAudioTags = _fixture.Create<bool>();
+            var allowVideoTags = _fixture.Create<bool>();
+
+            // Act
+            await _sut.WriteHeaderAsync(allowAudioTags, allowVideoTags, default);
+
+            // Assert
+            await _flvWriter.Received(1).WriteHeaderAsync(allowAudioTags, allowVideoTags, Arg.Any<CancellationToken>());
+        }
+
+        [Fact]
+        public async Task WriteHeaderAsync_Should_StopClient_When_ExceptionThrown()
+        {
+            // Arrange
+            var allowAudioTags = _fixture.Create<bool>();
+            var allowVideoTags = _fixture.Create<bool>();
+
+            _flvWriter.WriteHeaderAsync(allowAudioTags, allowVideoTags, Arg.Any<CancellationToken>())
+                      .Throws(new Exception());
+
+            // Act
+            await _sut.WriteHeaderAsync(allowAudioTags, allowVideoTags, default);
+
+            // Assert
+            _sut.UntilComplete().IsCompleted.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task WriteTagAsync_Should_WriteTag()
+        {
+            // Arrange
+            var tagHeader = _fixture.Create<FlvTagHeader>();
+            var payloadBufer = Substitute.For<Action<INetBuffer>>();
+
+            // Act
+            await _sut.WriteTagAsync(tagHeader, payloadBufer, default);
+
+            // Assert
+            await _flvWriter.Received(1).WriteTagAsync(tagHeader, payloadBufer, Arg.Any<CancellationToken>());
+        }
+
+        [Fact]
+        public async Task WriteTagAsync_Should_Stop_When_ExceptionThrown()
+        {
+            // Arrange
+            var tagHeader = _fixture.Create<FlvTagHeader>();
+            var payloadBufer = Substitute.For<Action<INetBuffer>>();
+            _flvWriter.WriteTagAsync(tagHeader, payloadBufer, Arg.Any<CancellationToken>()).Throws(new Exception());
+
+            // Act
+            await _sut.WriteTagAsync(tagHeader, payloadBufer, default);
+
+            // Assert
+            _sut.UntilComplete().IsCompleted.Should().BeTrue();
+        }
+    }
+}
