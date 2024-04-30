7b0f84592f29c25a9bc8ebce8d261c95adbcbe97
Joseph Tam <josephnhtam@gmail.com>
2024-04-30 23:27:44 +0800

    feat: add unit tests for HttpResponseStreamWriter and WebSocketStreamWriter

---
+++ test/LiveStreamingServerNet.Flv.Test/HttpClients/HttpResponseStreamWriter.Test.cs
@@ -0,0 +1,35 @@
+﻿using AutoFixture;
+using LiveStreamingServerNet.Flv.Internal.Contracts;
+using LiveStreamingServerNet.Flv.Internal.HttpClients;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+
+namespace LiveStreamingServerNet.Flv.Test.HttpClients
+{
+    public class HttpResponseStreamWriterTest
+    {
+        private readonly IFixture _fixture;
+        private readonly HttpResponse _response;
+        private readonly IStreamWriter _sut;
+
+        public HttpResponseStreamWriterTest()
+        {
+            _fixture = new Fixture();
+            _response = Substitute.For<HttpResponse>();
+            _sut = new HttpResponseStreamWriter(_response);
+        }
+
+        [Fact]
+        public async Task WriteAsync_ShouldWriteToResponseBodyWriter()
+        {
+            // Arrange
+            var buffer = _fixture.Create<ReadOnlyMemory<byte>>();
+
+            // Act
+            await _sut.WriteAsync(buffer, default);
+
+            // Assert
+            await _response.BodyWriter.Received(1).WriteAsync(buffer, Arg.Any<CancellationToken>());
+        }
+    }
+}
---
+++ test/LiveStreamingServerNet.Flv.Test/WebSocketClients/WebSocketStreamWriter.Test.cs
@@ -0,0 +1,35 @@
+﻿using AutoFixture;
+using LiveStreamingServerNet.Flv.Internal.Contracts;
+using LiveStreamingServerNet.Flv.Internal.WebSocketClients;
+using NSubstitute;
+using System.Net.WebSockets;
+
+namespace LiveStreamingServerNet.Flv.Test.WebSocketClients
+{
+    public class WebSocketStreamWriterTest
+    {
+        private readonly IFixture _fixture;
+        private readonly WebSocket _webSocket;
+        private readonly IStreamWriter _sut;
+
+        public WebSocketStreamWriterTest()
+        {
+            _fixture = new Fixture();
+            _webSocket = Substitute.For<WebSocket>();
+            _sut = new WebSocketStreamWriter(_webSocket);
+        }
+
+        [Fact]
+        public async Task WriteAsync_ShouldWriteToResponseBodyWriter()
+        {
+            // Arrange
+            var buffer = _fixture.Create<ReadOnlyMemory<byte>>();
+
+            // Act
+            await _sut.WriteAsync(buffer, default);
+
+            // Assert
+            await _webSocket.Received(1).SendAsync(buffer, WebSocketMessageType.Binary, true, Arg.Any<CancellationToken>());
+        }
+    }
+}
