dca7b0ed222d6d5c65222ee9eb7bfd0391e7a414
Joseph Tam <josephnhtam@gmail.com>
2024-05-01 20:02:15 +0800

    feat: add LiveStreamingServerNet.BasicDemo

---
+++ samples/LiveStreamingServerNet.BasicDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using LiveStreamingServerNet.Networking;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace LiveStreamingServerNet.BasicDemo
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            using var liveStreamingServer = LiveStreamingServerBuilder.Create()
+                .ConfigureLogging(options => options.AddConsole())
+                .Build();
+
+            using var cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                cts.Cancel();
+                e.Cancel = true;
+            };
+
+            await liveStreamingServer.RunAsync(
+                new ServerEndPoint(new IPEndPoint(IPAddress.Any, 1935), false), cts.Token);
+        }
+    }
+}
