c1ebb4bec281d9a82888d2450315c93e4766f39a
Joseph Tam <josephnhtam@gmail.com>
2024-05-01 21:43:34 +0800

    feat: add LiveStreamingServerNet.EventsDemo

---
+++ samples/LiveStreamingServerNet.EventsDemo/Program.cs
@@ -0,0 +1,100 @@
+﻿using LiveStreamingServerNet.Networking;
+using LiveStreamingServerNet.Networking.Contracts;
+using LiveStreamingServerNet.Rtmp.Contracts;
+using LiveStreamingServerNet.Utilities.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Collections.Concurrent;
+using System.Net;
+
+namespace LiveStreamingServerNet.EventsDemo
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            using var liveStreamingServer = LiveStreamingServerBuilder.Create()
+                .ConfigureRtmpServer(options =>
+                {
+                    options.Services.Configure<PublishingTimeLimiterConfig>(config =>
+                        config.PublishingTimeLimitSeconds = 60
+                    );
+
+                    options.AddStreamEventHandler<PublishingTimeLimiter>();
+                })
+                .ConfigureLogging(options => options.AddConsole())
+                .Build();
+
+            using var cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                cts.Cancel();
+                e.Cancel = true;
+            };
+
+            await liveStreamingServer.RunAsync(
+                new ServerEndPoint(new IPEndPoint(IPAddress.Any, 1935), false), cts.Token);
+        }
+    }
+
+    public class PublishingTimeLimiterConfig
+    {
+        public int PublishingTimeLimitSeconds { get; set; }
+    }
+
+    public class PublishingTimeLimiter : IRtmpServerStreamEventHandler, IDisposable
+    {
+        private readonly ConcurrentDictionary<uint, ITimer> _clientTimers = new();
+        private readonly IServer _server;
+        private readonly PublishingTimeLimiterConfig _config;
+
+        public PublishingTimeLimiter(IServer server, IOptions<PublishingTimeLimiterConfig> config)
+        {
+            _server = server;
+            _config = config.Value;
+        }
+
+        public void Dispose()
+        {
+            foreach (var timer in _clientTimers.Values)
+                timer.Dispose();
+
+            _clientTimers.Clear();
+        }
+
+        public ValueTask OnRtmpStreamPublishedAsync(
+            IEventContext context, uint clientId, string streamPath, IReadOnlyDictionary<string, string> streamArguments)
+        {
+            _clientTimers[clientId] = new Timer(async _ =>
+            {
+                var client = _server.GetClient(clientId);
+
+                if (client != null)
+                    await client.DisconnectAsync();
+            }, null, TimeSpan.FromSeconds(_config.PublishingTimeLimitSeconds), Timeout.InfiniteTimeSpan);
+
+            return ValueTask.CompletedTask;
+        }
+
+        public ValueTask OnRtmpStreamUnpublishedAsync(IEventContext context, uint clientId, string streamPath)
+        {
+            if (_clientTimers.TryRemove(clientId, out var timer))
+                timer.Dispose();
+
+            return ValueTask.CompletedTask;
+        }
+
+        public ValueTask OnRtmpStreamMetaDataReceivedAsync(
+            IEventContext context, uint clientId, string streamPath, IReadOnlyDictionary<string, object> metaData)
+            => ValueTask.CompletedTask;
+
+        public ValueTask OnRtmpStreamSubscribedAsync(
+            IEventContext context, uint clientId, string streamPath, IReadOnlyDictionary<string, string> streamArguments)
+            => ValueTask.CompletedTask;
+
+        public ValueTask OnRtmpStreamUnsubscribedAsync(IEventContext context, uint clientId, string streamPath)
+            => ValueTask.CompletedTask;
+    }
+}
