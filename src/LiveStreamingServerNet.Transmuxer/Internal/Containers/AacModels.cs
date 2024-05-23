3420eef81977dc98bf39ac2dfc917da7599e06b8
Joseph Tam <josephnhtam@gmail.com>
2024-05-23 23:53:29 +0800

    refactor: rename some classes and variables

--- src/LiveStreamingServerNet.Transmuxer/Internal/Containers/AacModels.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LiveStreamingServerNet.Transmuxer.Internal.Containers
-{
-    internal record AacSequenceHeader(
-        byte ObjectType,
-        byte SampleRateIndex,
-        byte ChannelConfig
-    );
-}
