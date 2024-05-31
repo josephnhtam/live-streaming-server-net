203b34776cc47447d3fd5b710ad281d27f8c78a1
Joseph Tam <josephnhtam@gmail.com>
2024-06-01 00:53:34 +0800

    fix: CalculateHmacSha256

---
+++ src/LiveStreamingServerNet.Utilities/Extensions/SecurityExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+
+namespace LiveStreamingServerNet.Utilities.Extensions
+{
+    public static class SecurityExtensions
+    {
+        public static byte[] CalculateHmacSha256(this byte[] bytes, byte[] key)
+        {
+            using var hmac = new HMACSHA256(key);
+            return hmac.ComputeHash(bytes);
+        }
+    }
+}
