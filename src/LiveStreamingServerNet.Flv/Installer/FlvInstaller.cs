15d0863e04af71f6c17c9d1889e9b087c7cbf219
Joseph Tam <josephnhtam@gmail.com>
2024-06-07 18:07:30 +0800

    chore: cleanup

--- src/LiveStreamingServerNet.Flv/Installer/FlvInstaller.cs
+++ src/LiveStreamingServerNet.Flv/Installer/FlvInstaller.cs
@@ -10,7 +10,6 @@ using LiveStreamingServerNet.Flv.Internal.WebSocketClients;
 using LiveStreamingServerNet.Flv.Internal.WebSocketClients.Contracts;
 using LiveStreamingServerNet.Flv.Middlewares;
 using LiveStreamingServerNet.Networking.Contracts;
-using LiveStreamingServerNet.Rtmp.Contracts;
 using LiveStreamingServerNet.Rtmp.Installer.Contracts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
