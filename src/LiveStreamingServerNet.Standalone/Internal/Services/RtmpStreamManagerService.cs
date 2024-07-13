3c90bdfbcaaeb22fa1865e7215b918307c5d70ff
Joseph Tam <josephnhtam@gmail.com>
2024-07-13 17:59:38 +0800

    fix: RtmpStreamManagerService

--- src/LiveStreamingServerNet.Standalone/Internal/Services/RtmpStreamManagerService.cs
+++ src/LiveStreamingServerNet.Standalone/Internal/Services/RtmpStreamManagerService.cs
@@ -8,7 +8,9 @@ namespace LiveStreamingServerNet.Standalone.Internal.Services
     internal class RtmpStreamManagerService : IRtmpStreamManagerService
     {
         private readonly IServer _server;
-        private readonly ConcurrentDictionary<string, RtmpPublishStream> _publishStreams = new();
+        private readonly object _syncLock = new();
+        private readonly Dictionary<string, RtmpPublishStream> _publishStreamMap = new();
+        private readonly Dictionary<string, List<IClientControl>> _pendingSubscribersMap = new();
         private readonly ConcurrentDictionary<string, string> _streamPaths = new();
 
         public RtmpStreamManagerService(IServer server)
@@ -24,21 +26,41 @@ namespace LiveStreamingServerNet.Standalone.Internal.Services
         public ValueTask RtmpStreamPublishedAsync(uint clientId, string streamPath, IReadOnlyDictionary<string, string> streamArguments)
         {
             var client = GetClient(clientId);
+
             if (client == null)
                 return ValueTask.CompletedTask;
 
-            var stream = new RtmpPublishStream(client, streamPath, streamArguments);
+            lock (_syncLock)
+            {
+                if (!_publishStreamMap.ContainsKey(streamPath))
+                {
+                    var stream = new RtmpPublishStream(client, streamPath, streamArguments);
 
-            if (_publishStreams.TryAdd(streamPath, stream))
-                _streamPaths[stream.Id] = streamPath;
+                    if (_pendingSubscribersMap.TryGetValue(streamPath, out var pendingSubscribers))
+                    {
+                        foreach (var subscriber in pendingSubscribers)
+                            stream.AddSubscriber(subscriber);
+
+                        _pendingSubscribersMap.Remove(streamPath);
+                    }
+
+                    _publishStreamMap[streamPath] = stream;
+                    _streamPaths[stream.Id] = streamPath;
+                }
+            }
 
             return ValueTask.CompletedTask;
         }
 
         public ValueTask RtmpStreamUnpublishedAsync(uint clientId, string streamPath)
         {
-            if (_publishStreams.TryRemove(streamPath, out var stream))
-                _streamPaths.TryRemove(stream.Id, out _);
+            lock (_syncLock)
+            {
+                if (_publishStreamMap.Remove(streamPath, out var stream))
+                {
+                    _streamPaths.TryRemove(stream.Id, out _);
+                }
+            }
 
             return ValueTask.CompletedTask;
         }
@@ -49,24 +71,57 @@ namespace LiveStreamingServerNet.Standalone.Internal.Services
             if (subscriberClient == null)
                 return ValueTask.CompletedTask;
 
-            if (_publishStreams.TryGetValue(streamPath, out var publishStream))
-                publishStream.AddSubscriber(subscriberClient);
+            lock (_syncLock)
+            {
+                if (_publishStreamMap.TryGetValue(streamPath, out var publishStream))
+                {
+                    publishStream.AddSubscriber(subscriberClient);
+                }
+                else
+                {
+                    if (_pendingSubscribersMap.TryGetValue(streamPath, out var pendingSubscribers))
+                    {
+                        pendingSubscribers.Add(subscriberClient);
+                    }
+                    else
+                    {
+                        _pendingSubscribersMap[streamPath] = new List<IClientControl> { subscriberClient };
+                    }
+                }
+            }
 
             return ValueTask.CompletedTask;
         }
 
         public ValueTask RtmpStreamUnsubscribedAsync(uint clientId, string streamPath)
         {
-            if (_publishStreams.TryGetValue(streamPath, out var publishStream))
-                publishStream.RemoveSubscriber(clientId);
+            lock (_syncLock)
+            {
+                if (_publishStreamMap.TryGetValue(streamPath, out var publishStream))
+                {
+                    publishStream.RemoveSubscriber(clientId);
+                }
+                else if (_pendingSubscribersMap.TryGetValue(streamPath, out var pendingSubscribers))
+                {
+                    pendingSubscribers.RemoveAll(x => x.ClientId == clientId);
+
+                    if (!pendingSubscribers.Any())
+                        _pendingSubscribersMap.Remove(streamPath);
+                }
+            }
 
             return ValueTask.CompletedTask;
         }
 
         public ValueTask RtmpStreamMetaDataReceivedAsync(uint clientId, string streamPath, IReadOnlyDictionary<string, object> metaData)
         {
-            if (_publishStreams.TryGetValue(streamPath, out var publishStream))
-                publishStream.UpdateMetaData(metaData);
+            lock (_syncLock)
+            {
+                if (_publishStreamMap.TryGetValue(streamPath, out var publishStream))
+                {
+                    publishStream.UpdateMetaData(metaData);
+                }
+            }
 
             return ValueTask.CompletedTask;
         }
@@ -78,12 +133,18 @@ namespace LiveStreamingServerNet.Standalone.Internal.Services
             if (streamPath == null)
                 return null;
 
-            return _publishStreams.GetValueOrDefault(streamPath);
+            lock (_syncLock)
+            {
+                return _publishStreamMap.GetValueOrDefault(streamPath);
+            }
         }
 
         public IList<IRtmpPublishStream> GetStreams()
         {
-            return _publishStreams.Values.ToList<IRtmpPublishStream>();
+            lock (_syncLock)
+            {
+                return _publishStreamMap.Values.ToList<IRtmpPublishStream>();
+            }
         }
     }
 }
