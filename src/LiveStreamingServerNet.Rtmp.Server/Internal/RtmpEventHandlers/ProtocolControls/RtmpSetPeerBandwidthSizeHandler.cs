473b1a9e79108ee943ad76ce6c5567e8a737583f
Joseph Tam <josephnhtam@gmail.com>
2024-09-22 21:37:56 +0800

    feat: ignore setPeerBandwidth protocol control

---
+++ src/LiveStreamingServerNet.Rtmp.Server/Internal/RtmpEventHandlers/ProtocolControls/RtmpSetPeerBandwidthSizeHandler.cs
@@ -0,0 +1,24 @@
+﻿using LiveStreamingServerNet.Rtmp.Internal.Contracts;
+using LiveStreamingServerNet.Rtmp.Internal.RtmpEventHandlers;
+using LiveStreamingServerNet.Rtmp.Internal.RtmpEventHandlers.Dispatcher.Attributes;
+using LiveStreamingServerNet.Rtmp.Internal.RtmpEventHandlers.Dispatcher.Contracts;
+using LiveStreamingServerNet.Rtmp.Server.Internal.Contracts;
+using LiveStreamingServerNet.Utilities.Buffers.Contracts;
+
+namespace LiveStreamingServerNet.Rtmp.Server.Internal.RtmpEventHandlers.ProtocolControls
+{
+    [RtmpMessageType(RtmpMessageType.SetPeerBandwidth)]
+    internal class RtmpSetPeerBandwidthHandler : IRtmpMessageHandler<IRtmpClientSessionContext>
+    {
+        public RtmpSetPeerBandwidthHandler() { }
+
+        public ValueTask<bool> HandleAsync(
+            IRtmpChunkStreamContext chunkStreamContext,
+            IRtmpClientSessionContext clientContext,
+            IDataBuffer payloadBuffer,
+            CancellationToken cancellationToken)
+        {
+            return ValueTask.FromResult(true);
+        }
+    }
+}
