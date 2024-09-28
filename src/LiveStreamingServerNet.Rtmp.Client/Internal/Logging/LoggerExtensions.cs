015bf20bef534485af446716e88de63c89fb4758
Joseph Tam <josephnhtam@gmail.com>
2024-09-29 02:46:56 +0800

    refactor: remove unused logging messages

--- src/LiveStreamingServerNet.Rtmp.Client/Internal/Logging/LoggerExtensions.cs
+++ src/LiveStreamingServerNet.Rtmp.Client/Internal/Logging/LoggerExtensions.cs
@@ -22,32 +22,8 @@ namespace LiveStreamingServerNet.Rtmp.Client.Internal.Logging
         [LoggerMessage(LogLevel.Error, "SessionId: {SessionId} | Handshake S2 Handling Failed")]
         public static partial void HandshakeS2HandlingFailed(this ILogger logger, uint sessionId);
 
-        [LoggerMessage(LogLevel.Error, "SessionId: {SessionId} | An error occurred while dispatching RTMP connected event")]
-        public static partial void DispatchingRtmpConnectedEventError(this ILogger logger, uint sessionId, Exception ex);
-
-        [LoggerMessage(LogLevel.Error, "SessionId: {SessionId} | An error occurred while dispatching RTMP connection rejected event")]
-        public static partial void DispatchingRtmpConnectionRejectedEventError(this ILogger logger, uint sessionId, Exception ex);
-
         [LoggerMessage(LogLevel.Error, "SessionId: {SessionId} | An error occurred while dispatching RTMP handshake complete event")]
         public static partial void DispatchingRtmpHandshakeCompleteEventError(this ILogger logger, uint sessionId, Exception ex);
-
-        [LoggerMessage(LogLevel.Error, "SessionId: {SessionId} | An error occurred while dispatching RTMP stream created event")]
-        public static partial void DispatchingRtmpStreamCreatedEventError(this ILogger logger, uint sessionId, Exception ex);
-
-        [LoggerMessage(LogLevel.Error, "SessionId: {SessionId} | An error occurred while dispatching RTMP stream metadata received event")]
-        public static partial void DispatchingRtmpStreamMetaDataReceivedEventError(this ILogger logger, uint sessionId, Exception ex);
-
-        [LoggerMessage(LogLevel.Error, "SessionId: {SessionId} | An error occurred while dispatching RTMP stream published event")]
-        public static partial void DispatchingRtmpStreamPublishedEventError(this ILogger logger, uint sessionId, Exception ex);
-
-        [LoggerMessage(LogLevel.Error, "SessionId: {SessionId} | An error occurred while dispatching RTMP stream subscribed event")]
-        public static partial void DispatchingRtmpStreamSubscribedEventError(this ILogger logger, uint sessionId, Exception ex);
-
-        [LoggerMessage(LogLevel.Error, "SessionId: {SessionId} | An error occurred while dispatching RTMP stream unpublished event")]
-        public static partial void DispatchingRtmpStreamUnpublishedEventError(this ILogger logger, uint sessionId, Exception ex);
-
-        [LoggerMessage(LogLevel.Error, "SessionId: {SessionId} | An error occurred while dispatching RTMP stream unsubscribed event")]
-        public static partial void DispatchingRtmpStreamUnsubscribedEventError(this ILogger logger, uint sessionId, Exception ex);
 
         [LoggerMessage(LogLevel.Trace, "SessionId: {SessionId} | Acknowledgement received")]
         public static partial void AcknowledgementReceived(this ILogger logger, uint sessionId);
