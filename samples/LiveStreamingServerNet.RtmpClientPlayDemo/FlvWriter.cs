a5aa20c1610ee880a2d96b4a9a1888085205a57c
Joseph Tam <josephnhtam@gmail.com>
2024-09-30 01:26:18 +0800

    feat: add rtmp client demo

---
+++ samples/LiveStreamingServerNet.RtmpClientPlayDemo/FlvWriter.cs
@@ -0,0 +1,69 @@
+﻿using LiveStreamingServerNet.Utilities.Buffers;
+using LiveStreamingServerNet.Utilities.Buffers.Contracts;
+
+namespace LiveStreamingServerNet.RtmpClientPlayDemo
+{
+    public class FlvWriter : IDisposable
+    {
+        private readonly BinaryWriter _streamWriter;
+
+        public FlvWriter(BinaryWriter streamWriter)
+        {
+            _streamWriter = streamWriter;
+        }
+
+        public void WriteHeader(bool allowAudioTags, bool allowVideoTags)
+        {
+            byte typeFlags = 0;
+
+            if (allowAudioTags)
+                typeFlags |= 0x04;
+
+            if (allowVideoTags)
+                typeFlags |= 0x01;
+
+            _streamWriter.Write([0x46, 0x4c, 0x56, 0x01, typeFlags, 0x00, 0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x00]);
+        }
+
+        public void WriteTag(FlvTagType tagType, uint timestamp, ReadOnlySpan<byte> payload)
+        {
+            using var dataBuffer = new DataBuffer();
+
+            var payloadSize = (uint)payload.Length;
+            var packageSize = payloadSize + FlvTagHeader.Size;
+
+            var header = new FlvTagHeader(tagType, payloadSize, timestamp);
+
+            header.Write(dataBuffer);
+            dataBuffer.Write(payload);
+            dataBuffer.WriteUInt32BigEndian(packageSize);
+            
+            _streamWriter.Write(dataBuffer.UnderlyingBuffer.AsSpan(0, dataBuffer.Size));
+        }
+
+        public void Dispose()
+        {
+            _streamWriter.Dispose();
+        }
+
+        private record struct FlvTagHeader(FlvTagType tagType, uint dataSize, uint timestamp)
+        {
+            public const int Size = 11;
+
+            public void Write(IDataBuffer dataBuffer)
+            {
+                dataBuffer.Write((byte)tagType);
+                dataBuffer.WriteUInt24BigEndian(dataSize);
+                dataBuffer.WriteUInt24BigEndian(timestamp);
+                dataBuffer.WriteUInt32BigEndian(0);
+            }
+        }
+    }
+
+    public enum FlvTagType : byte
+    {
+        Audio = 8,
+        Video = 9,
+        ScriptData = 18
+    }
+}
---
+++ samples/LiveStreamingServerNet.RtmpClientPlayDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using LiveStreamingServerNet.Rtmp;
+using LiveStreamingServerNet.Rtmp.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
+
+namespace LiveStreamingServerNet.RtmpClientPlayDemo
+{
+    public class Program
+    {
+        /// <summary>
+        /// Connect to rtmp://127.0.0.1/live/demo and save the stream to output.flv.
+        /// </summary>
+        public static async Task Main()
+        {
+            var rtmpClient = RtmpClientBuilder.Create()
+                .ConfigureLogging(options => options.AddConsole().SetMinimumLevel(LogLevel.Trace))
+                .Build();
+
+            var logger = rtmpClient.Services.GetRequiredService<ILogger<Program>>();
+
+            var serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1935);
+            var information = new Dictionary<string, object> { ["tcUrl"] = "rtmp://127.0.0.1/live" };
+            await rtmpClient.ConnectAsync(serverEndPoint, "live", information);
+
+            var rtmpStream = await rtmpClient.CreateStreamAsync();
+
+            using var fileStream = new FileStream("output.flv", FileMode.Create, FileAccess.Write, FileShare.Read);
+            using var binaryWriter = new BinaryWriter(fileStream);
+
+            using var flvWriter = new FlvWriter(binaryWriter);
+            flvWriter.WriteHeader(true, true);
+
+            rtmpStream.Subscribe.OnStreamMetaDataReceived += (sender, e) =>
+                logger.LogInformation($"Stream meta data received: {JsonSerializer.Serialize(e.StreamMetaData)}");
+
+            rtmpStream.Subscribe.OnVideoDataReceived += (sender, e) =>
+                flvWriter.WriteTag(FlvTagType.Video, e.Timestamp, e.RentedBuffer.AsSpan());
+
+            rtmpStream.Subscribe.OnAudioDataReceived += (sender, e) =>
+                flvWriter.WriteTag(FlvTagType.Audio, e.Timestamp, e.RentedBuffer.AsSpan());
+
+            rtmpStream.Subscribe.OnStatusReceived += (sender, e) =>
+            {
+                logger.LogInformation($"Status received: {e.Code}");
+
+                if (e.Code == RtmpStreamStatusCodes.PlayUnpublishNotify)
+                {
+                    rtmpClient.Stop();
+                }
+            };
+
+            rtmpStream.Subscribe.Play("demo");
+
+            await rtmpClient.UntilStoppedAsync();
+        }
+    }
+}
