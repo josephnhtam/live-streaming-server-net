e30ccbb861b3d38ed4b994ce247eb0df395cb669
Joseph Tam <josephnhtam@gmail.com>
2024-09-29 15:20:09 +0800

    feat: add RtmpUserControlMessageHandler

---
+++ src/LiveStreamingServerNet.Rtmp.Client/Internal/RtmpEventHandlers/UserControls/RtmpUserControlMessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using LiveStreamingServerNet.Rtmp.Internal.Contracts;
+using LiveStreamingServerNet.Rtmp.Internal.RtmpEventHandlers;
+using LiveStreamingServerNet.Rtmp.Internal.RtmpEventHandlers.Dispatcher.Attributes;
+using LiveStreamingServerNet.Rtmp.Internal.RtmpEventHandlers.Dispatcher.Contracts;
+using LiveStreamingServerNet.Rtmp.Client.Internal.Contracts;
+using LiveStreamingServerNet.Utilities.Buffers.Contracts;
+
+namespace LiveStreamingServerNet.Rtmp.Client.Internal.RtmpEventHandlers.UserControls
+{
+    [RtmpMessageType(RtmpMessageType.UserControlMessage)]
+    internal class RtmpUserControlMessageHandler : IRtmpMessageHandler<IRtmpSessionContext>
+    {
+        public ValueTask<bool> HandleAsync(
+            IRtmpChunkStreamContext chunkStreamContext,
+            IRtmpSessionContext context,
+            IDataBuffer payloadBuffer,
+            CancellationToken cancellationToken)
+        {
+            return ValueTask.FromResult(true);
+        }
+    }
+}
