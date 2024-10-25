d0d3f1b6e47ea7d9e70d802033dedec10debc9ec
Joseph Tam <josephnhtam@gmail.com>
2024-10-26 02:58:22 +0800

    fix: message interception should not consider timestamp offset

--- src/LiveStreamingServerNet.Rtmp.Server/Internal/Services/RtmpMediaCachingInterceptionService.cs
+++ src/LiveStreamingServerNet.Rtmp.Server/Internal/Services/RtmpMediaCachingInterceptionService.cs
@@ -4,6 +4,7 @@ using LiveStreamingServerNet.Rtmp.Server.Internal.Services.Contracts;
 using LiveStreamingServerNet.Utilities.Buffers.Contracts;
 using LiveStreamingServerNet.Utilities.Common;
 using LiveStreamingServerNet.Utilities.Common.Contracts;
+using System.Diagnostics;
 
 namespace LiveStreamingServerNet.Rtmp.Server.Internal.Services
 {
@@ -37,8 +38,10 @@ namespace LiveStreamingServerNet.Rtmp.Server.Internal.Services
 
                 try
                 {
+                    Debug.Assert(timestamp >= publishStreamContext.TimestampOffset);
+
                     foreach (var interceptor in _interceptors)
-                        await interceptor.OnCachePictureAsync(clientId, streamPath, mediaType, rentedBuffer, timestamp);
+                        await interceptor.OnCachePictureAsync(clientId, streamPath, mediaType, rentedBuffer, timestamp - publishStreamContext.TimestampOffset);
                 }
                 finally
                 {
