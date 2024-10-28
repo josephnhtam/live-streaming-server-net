1621d4828a29c12d8e432489095a9aa7cfe00a0d
Joseph Tam <josephnhtam@gmail.com>
2024-10-28 13:19:56 +0800

    feat: ignore messages with unknown type id

---
+++ src/LiveStreamingServerNet.Rtmp/Internal/Logging/LoggerExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LiveStreamingServerNet.Rtmp.Internal.Logging
+{
+    internal static partial class LoggerExtensions
+    {
+        [LoggerMessage(LogLevel.Trace, "Message handler not found (ChunkStreamId={ChunkStreamId}, MessageTypeId={MessageTypeId})")]
+        public static partial void MessageHandlerNotFound(this ILogger logger, uint chunkStreamId, byte messageTypeId);
+    }
+}
