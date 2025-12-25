5b05293a806fb8ce43c9ddea8ae2563fd7765d52
Joseph Tam <josephnhtam@gmail.com>
2025-12-25 23:25:00 +0800

    feat: add option to disable automatic unknown attributes handling

--- src/LiveStreamingServerNet.WebRTC/LiveStreamingServerNet.WebRTC/Internal/Stun/Configurations/StunClientConfiguration.cs
+++ src/LiveStreamingServerNet.WebRTC/LiveStreamingServerNet.WebRTC/Internal/Stun/Configurations/StunClientConfiguration.cs
@@ -4,25 +4,31 @@ namespace LiveStreamingServerNet.WebRTC.Internal.Stun.Configurations
     {
         /// <summary>
         /// Initial retransmission timeout in milliseconds.
-        /// Default 500ms.
+        /// Default: 500ms.
         /// </summary>
         public int RetransmissionTimeout { get; set; } = 500;
 
         /// <summary>
         /// Maximum retransmission timeout in milliseconds.
-        /// Default 1600ms.
+        /// Default: 1600ms.
         /// </summary>
         public int MaxRetransmissionTimeout { get; set; } = 1600;
 
         /// <summary>
         /// Max retransmissions.
-        /// Default 7.
+        /// Default: 7.
         /// </summary>
         public int MaxRetransmissions { get; set; } = 7;
 
         /// <summary>
         /// Transaction timeout factor.
-        /// Default 16.</summary>
+        /// Default: 16.</summary>
         public int TransactionTimeoutFactor { get; set; } = 16;
+
+        /// <summary>
+        /// Automatically handle unknown comprehension-required attributes by returning an 420 error response.
+        /// Default: true.
+        /// </summary>
+        public bool HandleUnknownComprehensionRequiredAttributes { get; set; } = true;
     }
 }
