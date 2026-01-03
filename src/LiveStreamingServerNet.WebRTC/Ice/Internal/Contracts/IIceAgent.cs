135d17dc24db89194678357afc100bf724f417b2
Joseph Tam <josephnhtam@gmail.com>
2026-01-03 19:43:06 +0800

    feat: IIceAgent interface

--- src/LiveStreamingServerNet.WebRTC/Ice/Internal/Contracts/IIceAgent.cs
+++ src/LiveStreamingServerNet.WebRTC/Ice/Internal/Contracts/IIceAgent.cs
@@ -2,6 +2,10 @@ namespace LiveStreamingServerNet.WebRTC.Ice.Internal.Contracts
 {
     internal interface IIceAgent : IAsyncDisposable
     {
+        bool Start();
+        ValueTask<bool> StopAsync();
+        void AddRemoteCandidate(RemoteIceCandidate? candidate);
         ValueTask<IIcePeerConnection> AcceptAsync(CancellationToken cancellation = default);
+        event EventHandler<IceCandidate?> OnLocalCandidateGathered;
     }
 }
